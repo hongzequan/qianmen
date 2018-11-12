14aef751e927e51b1f66d9a8e0a94e17460452bf
洪泽权 <625594687@qq.com>
2018-11-12 10:20:51 +0800

    js修改

--- js/index.js
+++ js/index.js
@@ -46,72 +46,71 @@ function stop_animation() {
     $('.Load_Animation').css('display', 'none');
 }
 
-$(".main_report_footer .footer_save").on('click',function(){
+$(".main_report_footer .footer_save").on('click', function() {
     layer.open({
         type: 0,
         content: "举报成功！<br/>您已行使了门前三包监督权力",
-        success: function () {//弹出层打开时的回调函数
+        success: function() { //弹出层打开时的回调函数
 
         },
-        end: function () {//弹出层销毁时的回调函数
+        end: function() { //弹出层销毁时的回调函数
             history.go(-1);
         }
     });
 });
 
-$("#false_report").on('click',function(){
+$("#false_report").on('click', function() {
     layer.open({
         title: "群众虚假举报？",
         content: '确定虚假举报后，巡查员将对该问题进行核实，若问题真实有效则扣双倍分数！',
-        btn: ['确定','取消'],
-        yes: function (index) {
+        btn: ['确定', '取消'],
+        yes: function(index) {
             console.log('点击确定时的回调函数');
             layer.close(index);
         },
-        no: function (index) {
+        no: function(index) {
             console.log('点击取消时的回调函数');
             layer.close(index);
         }
     });
 });
 
-$("#handle").on('click',function(){
+$("#handle").on('click', function() {
     layer.open({
         title: "确定自行处理？",
         content: '确定自行处理后，巡查员将对该问题进行复查，若问题仍旧存在则扣双倍分数！',
-        btn: ['确定','取消'],
-        yes: function (index) {
+        btn: ['确定', '取消'],
+        yes: function(index) {
             console.log('点击确定时的回调函数');
             layer.close(index);
         },
-        no: function (index) {
+        no: function(index) {
             console.log('点击取消时的回调函数');
             layer.close(index);
         }
     });
 });
 
-$("#evaluate").on('click',function(){
+$("#evaluate").on('click', function() {
     layer.open({
-        content:
-        '<div class="layer_appr">' +
+        content: '<div class="layer_appr">' +
             '<div class="content_appr">' +
-                '<span class="appr-laugh-icon"></span>' +
-                '<span class="appr-laugh-icon"></span>' +
-                '<span class="appr-laugh-icon"></span>' +
-                '<span class="appr-laugh-icon"></span>' +
-                '<span class="appr-cry-icon"></span>' +
+            '<span class="appr-laugh-icon"></span>' +
+            '<span class="appr-laugh-icon"></span>' +
+            '<span class="appr-laugh-icon"></span>' +
+            '<span class="appr-laugh-icon"></span>' +
+            '<span class="appr-cry-icon"></span>' +
             '</div>' +
             '<div class="layer_appr_text">' +
-                '<textarea placeholder="请输入评价内容"></textarea>' +
+            '<textarea placeholder="请输入评价内容"></textarea>' +
             '</div>' +
-        '</div>',
-        btn: ['确定','取消'],
-        yes: function (index) {
+            '</div>',
+        btn: ['确定', '取消'],
+        yes: function(index) {
             console.log('点击确定时的回调函数');
             layer.close(index);
         },
-        no: function (index) {
+        no: function(index) {
             console.log('点击取消时的回调函数');
             layer.close(index);
         }
@@ -127,54 +126,57 @@ $("#evaluate").on('click',function(){
 // });
 
 //群众举报－tab
-$('#people_tab .report_tab').find('a').click(function(){
+$('#people_tab .report_tab').find('a').click(function() {
     $(this).closest('.report_tab').find('a').removeClass('tab_active');
     $(this).addClass('tab_active');
     $(this).closest('.people_tab').find('.people_panel').removeClass('tab_active');
-    $(""+$(this).attr('data-href')).addClass('tab_active');
+    $("" + $(this).attr('data-href')).addClass('tab_active');
 });
 
 /* 百度地图调用方法 */
-function initMap(){
-    // 百度地图API功能
-    var map = new BMap.Map("allmap");    // 创建Map实例
-    map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
+function initMap() {
+    if ($('#allmap').length > 0) {
+        // 百度地图API功能
+        var map = new BMap.Map("allmap"); // 创建Map实例
+        map.centerAndZoom(new BMap.Point(116.404, 39.915), 11); // 初始化地图,设置中心点坐标和地图级别
 
-    function myFun(result){
-        var cityName = result.name;
-        map.setCenter(cityName);
-    }
-    var myCity = new BMap.LocalCity();
-    myCity.get(myFun);
-
-    var geolocation = new BMap.Geolocation();  //实例化浏览器定位对象。
-    geolocation.getCurrentPosition(function(r){   //定位结果对象会传递给r变量
-
-        if(this.getStatus() == BMAP_STATUS_SUCCESS){  //通过Geolocation类的getStatus()可以判断是否成功定位。
-
-            var mk = new BMap.Marker(r.point);    //基于定位的这个点的点位创建marker
-
-            map.addOverlay(mk);    //将marker作为覆盖物添加到map地图上
-
-            map.panTo(r.point);   //将地图中心点移动到定位的这个点位置。注意是r.point而不是r对象。
-
-        } else {
-            alert('failed'+this.getStatus());
+        function myFun(result) {
+            var cityName = result.name;
+            map.setCenter(cityName);
         }
+        var myCity = new BMap.LocalCity();
+        myCity.get(myFun);
 
-    },{enableHighAccuracy: true})
-    // map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
-    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+        var geolocation = new BMap.Geolocation(); //实例化浏览器定位对象。
+        geolocation.getCurrentPosition(function(r) { //定位结果对象会传递给r变量
+
+            if (this.getStatus() == BMAP_STATUS_SUCCESS) { //通过Geolocation类的getStatus()可以判断是否成功定位。
+
+                var mk = new BMap.Marker(r.point); //基于定位的这个点的点位创建marker
+
+                map.addOverlay(mk); //将marker作为覆盖物添加到map地图上
+
+                map.panTo(r.point); //将地图中心点移动到定位的这个点位置。注意是r.point而不是r对象。
+
+            } else {
+                alert('failed' + this.getStatus());
+            }
+
+        }, { enableHighAccuracy: true })
+        // map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
+        map.enableScrollWheelZoom(true); //开启鼠标滚轮缩放
+    }
+
 }
 
 // 获取当前地址信息
-function getAddress(){
-    var geolocation = new BMap.Geolocation();  //实例化浏览器定位对象。
-    geolocation.getCurrentPosition(function(r) {   //定位结果对象会传递给r变量
-        if (this.getStatus() == BMAP_STATUS_SUCCESS) {  //通过Geolocation类的getStatus()可以判断是否成功定位。
+function getAddress() {
+    var geolocation = new BMap.Geolocation(); //实例化浏览器定位对象。
+    geolocation.getCurrentPosition(function(r) { //定位结果对象会传递给r变量
+        if (this.getStatus() == BMAP_STATUS_SUCCESS) { //通过Geolocation类的getStatus()可以判断是否成功定位。
             var geoc = new BMap.Geocoder();
             var pt = r.point;
-            geoc.getLocation(pt, function (rs) {
+            geoc.getLocation(pt, function(rs) {
                 var addComp = rs.addressComponents;
                 document.getElementById('address').innerHTML = addComp.province + ", " + addComp.city + ", " + addComp.district + ", " + addComp.street + ", " + addComp.streetNumber;
             });
@@ -187,24 +189,28 @@ function getAddress(){
 //获取当前时间
 var date = new Date();
 var now = date.getTime();
+
 function countTime() {
     //设置截止时间
-    var end = now+30*24*3600;
+    var end = now + 30 * 24 * 3600;
 
     //时间差
-    var leftTime = end-(new Date().getTime());
+    var leftTime = end - (new Date().getTime());
     //定义变量 d,h,m,s保存倒计时的时间
-    var d,h,m,s;
-    if (leftTime>=0) {
-        d = Math.floor(leftTime/1000/60/60/24);
-        h = Math.floor(leftTime/1000/60/60%24);
-        m = Math.floor(leftTime/1000/60%60);
-        s = Math.floor(leftTime/1000%60);
+    var d, h, m, s;
+    if (leftTime >= 0) {
+        d = Math.floor(leftTime / 1000 / 60 / 60 / 24);
+        h = Math.floor(leftTime / 1000 / 60 / 60 % 24);
+        m = Math.floor(leftTime / 1000 / 60 % 60);
+        s = Math.floor(leftTime / 1000 % 60);
     }
-    //将倒计时赋值到div中
-    document.getElementById("_h").innerHTML = h>9?h:'0'+h;
-    document.getElementById("_m").innerHTML = m>9?m:'0'+m;
-    document.getElementById("_s").innerHTML = s>9?s:'0'+s;
+    if ($('#_h').length > 0) {
+        //将倒计时赋值到div中
+        document.getElementById("_h").innerHTML = h > 9 ? h : '0' + h;
+        document.getElementById("_m").innerHTML = m > 9 ? m : '0' + m;
+        document.getElementById("_s").innerHTML = s > 9 ? s : '0' + s;
+    }
+
     //递归每秒调用countTime方法，显示动态时间效果
-    setTimeout(countTime,1000);
+    setTimeout(countTime, 1000);
 }
